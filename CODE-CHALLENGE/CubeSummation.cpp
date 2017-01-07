283f8de214e381c70aab953d6c26ac7c68eb5b82
dicaballeror <dicaballeror@users.noreply.github.com>
2017-01-06 23:32:19 -0500

    Update CubeSummation.cpp

--- CODE-CHALLENGE/CubeSummation.cpp
+++ CODE-CHALLENGE/CubeSummation.cpp
@@ -5,8 +5,42 @@
 #include <algorithm>
 using namespace std;
 
+struct Operation {
+	int x, y, z;
+	long long w;
+	bool isInside ( int x1, int y1, int z1, int x2, int y2, int z2 ) {
+		return x1 <= x && x <= x2 && y1 <= y && y <= y2 && z1 <= z && z <= z2;
+	}
+};
+
+void doUpdate ( vector < Operation > &O ) {
+	// to do
+}
+
+void doQuery ( vector < Operation > &O ) {
+	// to do
+}
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    
+    int testCases;
+    int N, M;
+    
+    cin >> testCases;
+    
+    while ( testCases -- ) {
+    	
+    	vector < Operation > operations;
+    	string type;
+    	
+    	cin >> N >> M;
+    	
+    	for ( int i = 0; i < M; i ++ ) {
+    		cin >> type;
+    		if ( type == "UPDATE" ) doUpdate(operations);
+    		else doQuery(operations);
+			}
+		}
+    
     return 0;
 }
